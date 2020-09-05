42672601142b2b394040fada9c1eba723e7d1b0a
sbabel <babel.sneha11@gmail.com>
2020-09-05 14:08:41 +0530

    Firt time

---
+++ BasicCoreJava/src/VaribalesAndDataTypes/DataTypes.java
@@ -0,0 +1,23 @@
+package VaribalesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sneha");
+int age=32;
+String name="Sneha";
+boolean Mstatus=true;
+double height=5.8;
+
+System.out.println(age);
+
+
+//Reserved keywords
+//Difference between byte,short,long, int
+//difference between double and float
+//JDK ,JRE,JVM differences
+	}
+
+}
---
+++ BasicCoreJava/src/VaribalesAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,11 @@
+package VaribalesAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Java Class");
+
+	}
+
+}
